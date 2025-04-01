040d429338ea946dc125d9fe435273f5f64efee6
yingfeizhou <yingfeizhou@boyaa.com>
2025-04-02 00:33:44 +0800

    fix: 删除所有文件

--- android/src/newarch/TencentAsrSdkSpec.java
+++
@@ -1,9 +0,0 @@
-package com.tencentasrsdk;
-
-import com.facebook.react.bridge.ReactApplicationContext;
-
-abstract class TencentAsrSdkSpec extends NativeTencentAsrSdkSpec {
-  TencentAsrSdkSpec(ReactApplicationContext context) {
-    super(context);
-  }
-}
